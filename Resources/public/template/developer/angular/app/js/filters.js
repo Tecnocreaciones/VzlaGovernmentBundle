00b55fc6c273b6477d73b347e901a0a443421693
Carlos Mendoza <inhack20@gmail.com>
2014-09-26 14:50:06 -0430

    Añadiendo filtros

--- Resources/public/template/developer/angular/app/js/filters.js
+++ Resources/public/template/developer/angular/app/js/filters.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /* Filters */
 
 angular.module('mainApp.filters', []).
@@ -7,4 +6,19 @@ angular.module('mainApp.filters', []).
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
     }
-  }]);
+  }])
+  .filter('myDate',function($filter){
+      return function(dateString){
+          var mydate = new Date(dateString);
+          var dateFormated = $filter('date')(mydate, 'yyyy-MM-dd');
+          return dateFormated;
+      };
+  })
+  .filter('myDateTime',function($filter){
+      return function(dateString){
+          var mydate = new Date(dateString);
+          var dateFormated = $filter('date')(mydate, 'yyyy-MM-dd HH:mm a');
+          return dateFormated;
+      };
+  })
+  ;
